d04db621c5f3ad92f13653c88076f62cd90acd41
Luiz Gonçalves <luiz.goncalves@ufpr.br>
2018-01-09 13:50:34 -0200

    add gulp, npm and add some libraries

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends Controller {
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function index() {
+
+        return $this->render('index.html.twig');
+    }
+}
